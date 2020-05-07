556c8c0e955f3c769639b408eaa527f1b0f730b7
Udit Singh <udit@pop-os.localdomain>
2020-05-07 15:58:40 +0530

    ready for deployment

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+const path=require('path');
+
+const fs=require('fs');
  const express=require('express');
  const app=express();
  const bodyParser=require('body-parser');
 const refRoutes=require('./routes/referral');
  const mongoose=require('mongoose');
+ const helmet=require('helmet');
+const compression=require('compression');
+const morgan=require('morgan');
+
+
+const accessLogStream=fs.createWriteStream(path.join(__dirname,'access.log'),{flags:'a'});
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined',{stream:accessLogStream}));
 
 
  app.use(bodyParser.json()); // application/json
@@ -20,6 +33,6 @@ const refRoutes=require('./routes/referral');
 
   mongoose.connect('mongodb+srv://uditsingh294:5511@taskapi-9jegf.mongodb.net/test?retryWrites=true&w=majority')
 .then(result => {
-     app.listen(8080);
+     app.listen(process.env.PORT||3000);
    })
    .catch(err => console.log(err));
